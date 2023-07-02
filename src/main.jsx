b251d3f40723ad3377b330fb6c097c4bad29dc47
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-07-02 15:27:39 -0400

    Set up routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,25 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import InventoryViewAll from "./pages/InventoryViewAll";
+import InventoryViewOne from "./pages/InventoryViewOne";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/inventory" element={<InventoryViewAll />} />
+      <Route path="/inventory/:id" element={<InventoryViewOne />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
