fa9117124fb1ea4b427e268a9dff2e91a71594e2
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-07-07 11:26:13 -0400

    Calculate pairs & value of collection from api

--- src/components/ShoeCollectionItem.jsx
+++ src/components/ShoeCollectionItem.jsx
@@ -1,28 +1,27 @@
+import { Collections } from "@mui/icons-material";
 import { ListGroupItem } from "react-bootstrap";
-import PropTypes from "prop-types";
 
 function ShoeCollectionItem({ collection }) {
+  const getValue = (collection) => {
+    let total = 0;
+    collection.shoes.forEach((shoe) => {
+      total += shoe.shoePrice;
+    });
+    return total;
+  };
+
   return (
     <ListGroupItem
       className=" list-group-item list-group-item-action flex-column align-items-start"
       collection={collection.id}
     >
-      <h5>{collection.title}</h5>
+      <h5>{collection.shoeCollectionName}</h5>
       <p>
-        Pairs: {collection.pairs}, Value: ${collection.value}
+        Pairs: {collection.shoes?.length ?? 0}, Value: $
+        {collection.shoes ? getValue(collection) : 0}
       </p>
     </ListGroupItem>
   );
 }
-
-// Temporary fix for ESLint error:
-ShoeCollectionItem.propTypes = {
-  collection: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    pairs: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-  }).isRequired,
-};
 
 export default ShoeCollectionItem;
