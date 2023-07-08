37cb237728268083da881c1ac57eb7285035ddab
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-07-08 14:31:22 -0400

    Implement update collection dispatch

--- src/components/ShoeCollectionItem.jsx
+++ src/components/ShoeCollectionItem.jsx
@@ -1,8 +1,21 @@
-import { Collections } from "@mui/icons-material";
-import { ListGroupItem, Button } from "react-bootstrap";
+import { Collections, Save } from "@mui/icons-material";
+import { ListGroupItem, Button, Form } from "react-bootstrap";
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useDeleteCollectionMutation } from "../slices/inventoryApiSlice";
+import { useGetCollectionsMutation } from "../slices/inventoryApiSlice";
+import { useUpdateCollectionMutation } from "../slices/inventoryApiSlice";
+import { useSelector } from "react-redux";
 
-function ShoeCollectionItem({ collection }) {
+function ShoeCollectionItem({ collection, handleNewCollection }) {
+  const [activatedCollection, setActivatedCollection] = useState(false);
+  const [editCollection, setEditCollection] = useState(false);
+  const dispatch = useDispatch();
+  const [deleteCollection, { isLoading }] = useDeleteCollectionMutation();
+  const [updateCollection, { isLoading: isUpdating }] =
+    useUpdateCollectionMutation();
+  const token = useSelector((state) => state.auth.userInfo.token);
+
   const getValue = (collection) => {
     let total = 0;
     collection.shoes.forEach((shoe) => {
@@ -11,10 +24,32 @@ function ShoeCollectionItem({ collection }) {
     return total;
   };
 
-  const [activatedCollection, setActivatedCollection] = useState(false);
+  const handleEditCollection = (event) => {
+    event.stopPropagation(); // Prevents the click event from bubbling up to the parent element
+    setEditCollection(!editCollection);
+  };
 
   const handleActivate = () => {
     setActivatedCollection(!activatedCollection);
+  };
+
+  const handleSaveCollection = (event) => {
+    // Check if the text input is empty
+    const collectionName = document.querySelector(".collection-rename");
+    console.log(collectionName.value);
+    console.log(collection.id);
+    console.log(token);
+    if (collectionName.value === "") {
+      alert("Please enter a collection name");
+      return;
+    }
+    updateCollection({
+      shoeCollectionName: collectionName.value,
+      collectionId: collection.id,
+      token: token,
+    });
+    handleNewCollection();
+    setEditCollection(!editCollection);
   };
 
   return (
@@ -23,16 +58,34 @@ function ShoeCollectionItem({ collection }) {
       collection={collection.id}
       onClick={handleActivate}
     >
-      <h5>{collection.shoeCollectionName}</h5>
+      {editCollection ? (
+        <Form.Control
+          type="text"
+          placeholder={collection.shoeCollectionName}
+          size="lg"
+          className="collection-rename"
+        />
+      ) : (
+        <h5>{collection.shoeCollectionName}</h5>
+      )}
       <p>
         Pairs: {collection.shoes?.length ?? 0}, Value: $
         {collection.shoes ? getValue(collection) : 0}
       </p>
       {activatedCollection && (
         <div>
-          <Button variant="primary">View</Button>
-          <Button variant="secondary">Edit</Button>
-          <Button variant="danger">Delete</Button>
+          {!editCollection && <Button variant="primary">View</Button>}
+          <Button
+            variant="secondary"
+            onClick={
+              !editCollection ? handleEditCollection : handleSaveCollection
+            }
+          >
+            {!editCollection ? "Edit" : "Save"}
+          </Button>
+          <Button variant="danger" onClick={handleEditCollection}>
+            {!editCollection ? "Delete" : "Cancel"}
+          </Button>
         </div>
       )}
     </ListGroupItem>
