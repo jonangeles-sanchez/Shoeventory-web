46432fc43aec160f307faeba22de22b4f09528ee
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-07-08 13:07:24 -0400

    Toggle view,edit,delete btns

--- src/components/ShoeCollectionItem.jsx
+++ src/components/ShoeCollectionItem.jsx
@@ -1,5 +1,6 @@
 import { Collections } from "@mui/icons-material";
-import { ListGroupItem } from "react-bootstrap";
+import { ListGroupItem, Button } from "react-bootstrap";
+import { useState } from "react";
 
 function ShoeCollectionItem({ collection }) {
   const getValue = (collection) => {
@@ -10,16 +11,30 @@ function ShoeCollectionItem({ collection }) {
     return total;
   };
 
+  const [activatedCollection, setActivatedCollection] = useState(false);
+
+  const handleActivate = () => {
+    setActivatedCollection(!activatedCollection);
+  };
+
   return (
     <ListGroupItem
       className=" list-group-item list-group-item-action flex-column align-items-start"
       collection={collection.id}
+      onClick={handleActivate}
     >
       <h5>{collection.shoeCollectionName}</h5>
       <p>
         Pairs: {collection.shoes?.length ?? 0}, Value: $
         {collection.shoes ? getValue(collection) : 0}
       </p>
+      {activatedCollection && (
+        <div>
+          <Button variant="primary">View</Button>
+          <Button variant="secondary">Edit</Button>
+          <Button variant="danger">Delete</Button>
+        </div>
+      )}
     </ListGroupItem>
   );
 }
